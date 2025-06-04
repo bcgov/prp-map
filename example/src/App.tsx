339fec7fc68f170e7f6f1a4e24c4098331c08573
marcellmueller <marcel@button.is>
2025-06-04 11:31:23 -0700

    chore: test pr merge reqs

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,10 +1,9 @@
-import React from "react";
 import { VectorFeatureMap } from "../../src/index";
 
 const App = () => {
   return (
     <div>
-      <h1>PRP Maps</h1>
+      <h1>PRP Maps test</h1>
       <VectorFeatureMap style={{ width: "100%", height: "500px" }} />
     </div>
   );
