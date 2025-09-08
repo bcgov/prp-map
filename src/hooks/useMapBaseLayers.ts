72dbb9359e05a32d3ecc4aa38cd7b5863b8bfa1f
jimmypalelil <jimmy.palelil@gov.bc.ca>
2025-09-08 08:29:24 -0700

    fix: create new base layer instances in useMapBaseLayers hook

--- src/hooks/useMapBaseLayers.ts
+++ src/hooks/useMapBaseLayers.ts
@@ -1,30 +1,10 @@
-import { useEffect } from "react";
+import { useEffect, useMemo } from "react";
 import VectorTileLayer from "ol/layer/VectorTile";
 import VectorTileSource from "ol/source/VectorTile";
 import MVT from "ol/format/MVT";
 import { applyStyle } from "ol-mapbox-style";
 import { useGetMapStyles } from "@/hooks";
 import { MAP_URLS } from "@/constants";
-
-// Create base layers with vector tile source and tuned settings
-const baseLayers = [
-  new VectorTileLayer({
-    declutter: true,
-    renderMode: "hybrid",
-    renderBuffer: 100,
-    updateWhileAnimating: true,
-    // Preload controls how many zoom levels beyond the current zoom level OpenLayers will
-    // load tiles for in advance at the cost of memory and cpu. Reduces white tiles while panning/zoom
-    preload: 2,
-    updateWhileInteracting: true,
-    source: new VectorTileSource({
-      format: new MVT(),
-      url: MAP_URLS.baseLayer,
-      wrapX: true,
-      cacheSize: 1024,
-    }),
-  }),
-];
 
 /**
  * Hook that creates and manages vector tile base layers
@@ -33,13 +13,35 @@ const baseLayers = [
 export const useMapBaseLayers = () => {
   const { data: glStyles } = useGetMapStyles();
 
+  const baseLayers = useMemo(
+    () => [
+      new VectorTileLayer({
+        declutter: true,
+        renderMode: "hybrid",
+        renderBuffer: 100,
+        updateWhileAnimating: true,
+        // Preload controls how many zoom levels beyond the current zoom level OpenLayers will
+        // load tiles for in advance at the cost of memory and cpu. Reduces white tiles while panning/zooming.
+        preload: 2,
+        updateWhileInteracting: true,
+        source: new VectorTileSource({
+          format: new MVT(),
+          url: MAP_URLS.baseLayer,
+          wrapX: true,
+          cacheSize: 1024,
+        }),
+      }),
+    ],
+    []
+  );
+
   useEffect(() => {
     if (glStyles) {
       baseLayers.forEach((baseLayer) => {
         applyStyle(baseLayer, glStyles, "esri");
       });
     }
-  }, [glStyles]);
+  }, [glStyles, baseLayers]);
 
   return baseLayers;
 };
